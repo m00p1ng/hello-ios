ec8fdbf2ba229a1fbb31e61f56bb83018f6082b5
m00p1ng <mongkonchai4412@gmail.com>
2021-03-07 20:13:20 +0700

    add egg counter

---
+++ 06-EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0
+        secondPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondPassed < totalTime {
+            secondPassed += 1
+            let percentageProgress = Float(secondPassed) / Float(totalTime)
+            
+            progressBar.progress = percentageProgress
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
+}
