3e427c52361705377d997200e3caab649af59535
m00p1ng <mongkonchai4412@gmail.com>
2021-03-07 23:10:29 +0700

    add bmi

---
+++ 08-BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by UnnamE on 7/3/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
